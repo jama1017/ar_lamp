72925372ed8f35249558e719cfa7e8723a9ae947
Jiaju Ma <majiaju@hotmail.com>
2019-04-28 00:41:18 -0400

    can continuously rotate. Need to fine tune min and hr angles

--- js/lamp.js
+++ js/lamp.js
@@ -19,7 +19,6 @@ function main() {
         min_hand = $('#min_hand');
         sec_hand = $('#sec_hand');
 
-
     let datetime = new Date(),
         h = datetime.getHours(),
         m = datetime.getMinutes(),
@@ -30,52 +29,44 @@ function main() {
     let anHour = aMinute * 60;
 
     console.log(datetime);
-    console.log(getCurrHourAngle(12));
 
-    // setCSSAngles(hr_hand, min_hand, sec_hand);
-    //
-    let hourTL = new TimelineMax({repeat: -1});
-    TweenMax.set(hr_hand, {
+    let currHrAngle = getCurrHourAngle(h),
+        currMinAngle = getCurrMinuteAngle(m),
+        currSecAngle = getCurrSecAngle(s)
+
+    let initTL = new TimelineMax();
+    initTL.set(hr_hand, {
         rotation: getCurrHourAngle(h),
         transformOrigin:"bottom center"
-    });
-
-    // hourTL.to(hr_hand, 1, {
-    //       rotation: 30,
-    //       transformOrigin:"bottom left",
-    //       ease:Linear.easeNone
-    //   });
-
-
-    let minTL = new TimelineMax({repeat: -1});
-    TweenMax.set(min_hand, {
+    })
+    .set(min_hand, {
         rotation: getCurrMinuteAngle(m),
         transformOrigin:"bottom center"
-    });
-    // minTL.to(min_hand, 60, {
-    //       rotation: 360,
-    //       transformOrigin:"bottom center",
-    //       ease:Linear.easeNone
-    //   });
-
-    let secTL = new TimelineMax({repeat: -1});
-    secTL.set(sec_hand, {
+    })
+    .set(sec_hand, {
         rotation: getCurrSecAngle(s),
         transformOrigin:"bottom center"
     });
-    // TweenMax.to(sec_hand, 1, {
-    //       rotation: 360,
-    //       transformOrigin:"bottom center",
-    //       ease:SteppedEase.config(60),
-    //       repeat: -1
-    //   });
+
+    TweenMax.to(hr_hand, anHour, {
+          rotation: currHrAngle + 360,
+          ease:Linear.easeNone,
+          repeat: -1
+      });
+
+    TweenMax.to(min_hand, aMinute, {
+          rotation: currMinAngle + 360,
+          ease:Linear.easeNone,
+          repeat: -1
+      });
+
+    TweenMax.to(sec_hand, aSecond, {
+          rotation: currSecAngle + 360,
+          ease:SteppedEase.config(60),
+          repeat: -1
+      });
 
 }
-
-// function setCSSAngles(hr, min, sec) {
-//     hr.css("transform-origin", "center center");
-//     hr.css("transform", "rotate(90deg)")
-// }
 
 function getCurrHourAngle(currHour) {
     if (currHour > 12) {
@@ -91,6 +82,5 @@ function getCurrMinuteAngle(currMinute) {
 }
 
 function getCurrSecAngle(currSec) {
-    // console.log(currSec * 6);
     return currSec * 6;
 }
